b5f77f2cc29e9c5d48454c4ef97b64b16ae09b26
Eli Caraballo <eli.caraballojr@gmail.com>
2020-06-08 23:02:40 -0500

    MVP completed

--- client/components/weaponsDisplay.jsx
+++ client/components/weaponsDisplay.jsx
@@ -12,49 +12,56 @@ const WeaponsDisplay = ({ guns }) => {
   return (
     <div>
       {
-      guns.length === 0 ? <h1>Select Weapon Type</h1>
-        : guns.map((gun) => (
-          // eslint-disable-next-line no-unused-expressions
-          <div key={gun.hash} onClick={() => { details === gun.hash ? setDetails('') : setDetails(gun.hash); }}>
-            {
-            details !== gun.hash ? (
-              <div>
-                <img src={`https://www.bungie.net/${gun.displayProperties.icon}`} alt="Weapon Icon" />
-                <div>{gun.displayProperties.name}</div>
-              </div>
-            )
-              : null
-            }
-            {
-              details === gun.hash ? (
-                <div className="details">
-                  <img src={`https://www.bungie.net/${gun.screenshot}`} alt="Weapon" />
-                  <div className="fadebox">
-                    <h2>{gun.displayProperties.name}</h2>
-                    <h3>{gun.displayProperties.description}</h3>
-                    {
-                      // eslint-disable-next-line consistent-return
-                      gun.investmentStats.map((item) => {
-                        if (item.value > 0) {
-                          // eslint-disable-next-line no-bitwise
-                          const id = item.statTypeHash >> 32;
-                          return (
-                            <div>
-                              <div>{`${stats[id].displayProperties.name}: ${stats[id].displayProperties.description}`}</div>
-                              <div>{item.value}</div>
-                            </div>
-                          );
-                        }
-                      })
-                    }
-                  </div>
+      guns.length === 0 ? <h1 className="temp">Select Weapon Type</h1> : null
+      }
+      <div className="weapons">
+        {
+        guns.length === 0 ? null
+          : guns.map((gun) => (
+            // eslint-disable-next-line no-unused-expressions
+            <div key={gun.hash} onClick={() => { details === gun.hash ? setDetails('') : setDetails(gun.hash); }}>
+              {
+              details !== gun.hash ? (
+                <div className="item">
+                  <img className="icon" src={`https://www.bungie.net/${gun.displayProperties.icon}`} alt="Weapon Icon" />
+                  <div>{gun.displayProperties.name}</div>
                 </div>
               )
                 : null
-            }
-          </div>
-        ))
-      }
+              }
+              {
+                details === gun.hash ? (
+                  <div className="details">
+                    <img src={`https://www.bungie.net/${gun.screenshot}`} alt="Weapon" />
+                    <div className="fadebox">
+                      Stats:
+                      <h2 className="name">{gun.displayProperties.name}</h2>
+                      <h3 className="description">{gun.displayProperties.description}</h3>
+                      <div className="allstats">
+                        {
+                          // eslint-disable-next-line consistent-return
+                          gun.investmentStats.map((item) => {
+                            if (item.value > 0) {
+                              // eslint-disable-next-line no-bitwise
+                              const id = item.statTypeHash >> 32;
+                              return (
+                                <div key={stats[id].index} className="stat">
+                                  <div>{`${stats[id].displayProperties.name}: ${item.value}`}</div>
+                                </div>
+                              );
+                            }
+                          })
+                        }
+                      </div>
+                    </div>
+                  </div>
+                )
+                  : null
+              }
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 };
--- client/index.jsx
+++ client/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable import/extensions */
 import React from 'react';
@@ -18,7 +19,6 @@ class App extends React.Component {
 
   searchClick(type, rarity) {
     if (type !== '' && rarity !== '') {
-      console.log(`${rarity} ${type}`);
       axios.get(`/getgunswithrarity/${rarity} ${type}`)
         .then((data) => {
           this.setState({
@@ -46,34 +46,40 @@ class App extends React.Component {
 
     return (
       <div>
-        <h1>DESTINY 2 GUN FINDER</h1>
+        <h1 className="title">DESTINY 2</h1>
+        <img className="D2image" src="D2.png" alt="D2" />
+        <h1 className="smalltext">WEAPON FINDER</h1>
         <div id="header">
-          <label>WEAPON TYPE:</label>
-          <select onChange={(event) => { this.handleChange(event); }} id="weaponType">
-            <option value="">--Select Weapon Type--</option>
-            <option value="Auto Rifle">Auto Rifles</option>
-            <option value="Combat Bow">Bows</option>
-            <option value="Fusion Rifle">Fusion Rifles</option>
-            <option value="Grenade Launcher">Grenade Launchers</option>
-            <option value="Hand Cannon">Hand Cannons</option>
-            <option value="Machine Gun">Machine Guns</option>
-            <option value="Pulse Rifle">Pulse Rifles</option>
-            <option value="Rocket Launcher">Rocket Launchers</option>
-            <option value="Scout Rifle">Scout Rifles</option>
-            <option value="Shotgun">Shotguns</option>
-            <option value="Sidearm">Sidearms</option>
-            <option value="Sniper Rifle">Sniper Rifles</option>
-            <option value="Submachine Gun">Submachine Guns</option>
-            <option value="Sword">Swords</option>
-          </select>
-          <label>Rarity</label>
-          <select onChange={(event) => { this.handleChange(event); }} id="weaponRarity">
-            <option value="">--Select Weapon Rarity--</option>
-            <option value="Exotic">Exotic</option>
-            <option value="Legendary">Legendary</option>
-            <option value="Rare">Rare</option>
-            <option value="Common">Common</option>
-          </select>
+          <div>
+            <label>WEAPON TYPE:</label>
+            <select onChange={(event) => { this.handleChange(event); }} id="weaponType">
+              <option value="">--Select Weapon Type--</option>
+              <option value="Auto Rifle">Auto Rifles</option>
+              <option value="Combat Bow">Bows</option>
+              <option value="Fusion Rifle">Fusion Rifles</option>
+              <option value="Grenade Launcher">Grenade Launchers</option>
+              <option value="Hand Cannon">Hand Cannons</option>
+              <option value="Machine Gun">Machine Guns</option>
+              <option value="Pulse Rifle">Pulse Rifles</option>
+              <option value="Rocket Launcher">Rocket Launchers</option>
+              <option value="Scout Rifle">Scout Rifles</option>
+              <option value="Shotgun">Shotguns</option>
+              <option value="Sidearm">Sidearms</option>
+              <option value="Sniper Rifle">Sniper Rifles</option>
+              <option value="Submachine Gun">Submachine Guns</option>
+              <option value="Sword">Swords</option>
+            </select>
+          </div>
+          <div>
+            <label>Rarity</label>
+            <select onChange={(event) => { this.handleChange(event); }} id="weaponRarity">
+              <option value="">--Select Weapon Rarity--</option>
+              <option value="Exotic">Exotic</option>
+              <option value="Legendary">Legendary</option>
+              <option value="Rare">Rare</option>
+              <option value="Common">Common</option>
+            </select>
+          </div>
           <button onClick={() => { this.searchClick(weaponType, weaponRarity); }} type="button">SEARCH</button>
         </div>
         <WeaponsDisplay guns={guns} />
